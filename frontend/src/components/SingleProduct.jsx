af50f85f3eb6a78d1cccab360c7d9373eb73ff52
BalavigneshRajasekar <you@example.com>
2024-09-26 00:35:08 +0530

    Single product view done

--- frontend/src/components/SingleProduct.jsx
+++ frontend/src/components/SingleProduct.jsx
@@ -1,10 +1,19 @@
 /* eslint-disable no-unused-vars */
 
-import { Container, Paper, Typography } from "@mui/material";
+import {
+  Button,
+  Container,
+  Divider,
+  Paper,
+  Rating,
+  Typography,
+} from "@mui/material";
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router";
-import Nav from "./Nav";
+
 import { productContext } from "../Context";
+import { Checkbox } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
 
 function SingleProduct() {
   const { id } = useParams();
@@ -17,14 +26,106 @@ function SingleProduct() {
     });
 
     setSingleProduct({ ...data });
+    console.log(singleProduct);
   }, []);
+  const Check = () => {};
   return (
     <>
       <Container>
         <Paper elevation={4}>
-          <Typography variant="h3">Product Details</Typography>
-          <Typography>Product ID: {id}</Typography>
-          <Typography>Product ID:{singleProduct.name} </Typography>
+          <div className="d-md-flex">
+            <div className="">
+              <img
+                src={singleProduct.image}
+                style={{ width: "300px", height: "400px" }}
+                className="p-3"
+              ></img>
+            </div>
+            <div>
+              <Typography variant="h3" className="fw-lighter p-3">
+                {singleProduct.name}
+              </Typography>
+              <Divider textAlign="left">LEFT</Divider>
+              <Rating
+                value={singleProduct.rating}
+                readOnly
+                size="medium"
+                color="primary"
+                className="ps-3"
+              />
+              <Typography className="p-2" variant="h6">
+                Description : {singleProduct.description}
+              </Typography>
+              <Typography className="p-2" variant="h4" sx={{ color: "red" }}>
+                Price : {singleProduct.price + "/-"}
+              </Typography>
+              <Typography className="p-2" variant="h6" sx={{ color: "grey" }}>
+                MRP incl. of all taxes
+              </Typography>
+              <Typography className="p-2" variant="h6" sx={{ color: "grey" }}>
+                Color : <span>{singleProduct.color}</span>
+              </Typography>
+              <Typography className="p-2" variant="h6" sx={{ color: "black" }}>
+                Size :
+              </Typography>
+              {/* <div className="ms-5">
+                {singleProduct.size.map((sizes, index) => (
+                  <>
+                    {sizes}
+                    <Checkbox
+                      key={index}
+                      value={sizes}
+                      onClick={(e) => Check(e)}
+                    />
+                  </>
+                ))}
+              </div> */}
+              <Typography variant="h6">Quantity :</Typography>
+              <div className="">
+                <select
+                  style={{
+                    width: "100px",
+                    outline: "none",
+                    border: "1px solid grey",
+                    height: "30px",
+                    borderRadius: "5px",
+                    marginTop: "5px",
+                    color: "orange",
+                    fontWeight: 700,
+                    textAlign: "center",
+                    marginLeft: "70px",
+                  }}
+                  defaultValue="1"
+                >
+                  {Array.from(
+                    { length: singleProduct.stock },
+                    (_, index1) => index1 + 1
+                  ).map((quan, index2) => (
+                    <option value={quan} key={index2}>
+                      {quan}
+                    </option>
+                  ))}
+                  ;
+                </select>
+                <Button
+                  variant="outlined"
+                  startIcon={<ShoppingCart />}
+                  sx={{ marginLeft: "50px" }}
+                >
+                  Add to Cart
+                </Button>
+              </div>
+              <div style={{ width: "300px", padding: "20px" }}>
+                <Button
+                  variant="contained"
+                  color="success"
+                  sx={{ width: "100%", padding: "10px", marginX: "70px" }}
+                >
+                  Buy It now{" "}
+                </Button>
+              </div>
+            </div>
+          </div>
         </Paper>
       </Container>
     </>
