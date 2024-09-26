c10d9a91854803cea34a04c86d5f6e2153d5c20c
BalavigneshRajasekar <you@example.com>
2024-09-26 19:28:07 +0530

    Back Btn added

--- frontend/src/components/SingleProduct.jsx
+++ frontend/src/components/SingleProduct.jsx
@@ -10,8 +10,10 @@ import {
 } from "@mui/material";
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router";
-import axios, { Axios } from "axios";
-
+import axios from "axios";
+import { Spin } from "antd";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { productContext } from "../Context";
 import { Checkbox } from "@mui/material";
 import { ShoppingCart } from "@mui/icons-material";
@@ -23,6 +25,9 @@ function SingleProduct() {
 
   useEffect(() => {
     fetchSingleProduct();
+    return () => {
+      setSingleProduct([]);
+    };
   }, []);
 
   const fetchSingleProduct = async () => {
@@ -40,111 +45,124 @@ function SingleProduct() {
   return (
     <>
       <Container>
-        <Paper elevation={4}>
-          <div className="d-flex gap-4 flex-column flex-md-row p-3">
-            <div className="">
-              <img
-                src={singleProduct.image}
-                style={{ width: "100%", height: "400px" }}
-                className="p-3"
-              ></img>
-            </div>
-            <div>
-              <Typography variant="h3" className="fw-lighter p-3">
-                {singleProduct.name}
-              </Typography>
-              <Divider textAlign="left">
-                {" "}
-                <Rating
-                  value={singleProduct.rating}
-                  readOnly
-                  size="medium"
-                  color="primary"
-                  className="ps-3"
-                />
-              </Divider>
-
-              <Typography className="p-2" variant="h6">
-                Description : {singleProduct.description}
-              </Typography>
-              <Typography className="p-2" variant="h4" sx={{ color: "red" }}>
-                Price : {singleProduct.price + "/-"}
-              </Typography>
-              <Typography className="p-2" variant="h6" sx={{ color: "grey" }}>
-                MRP incl. of all taxes
-              </Typography>
-              <Typography className="p-2" variant="h6" sx={{ color: "grey" }}>
-                Color :{" "}
-                <span style={{ color: singleProduct.color }}>
-                  {singleProduct.color}
-                </span>
-              </Typography>
-              <Typography className="p-2" variant="h6" sx={{ color: "grey" }}>
-                Category : {singleProduct.category}
-              </Typography>
-              <Typography className="p-2" variant="h6" sx={{ color: "black" }}>
-                Size :
-              </Typography>
-              <div className="ms-5">
-                {singleProduct.size &&
-                  singleProduct.size.map((sizes, index) => (
-                    <>
-                      {sizes}
-                      <Checkbox
-                        key={index}
-                        value={sizes}
-                        onClick={(e) => Check(e)}
-                      />
-                    </>
-                  ))}
-              </div>
-              <Typography variant="h6">Quantity :</Typography>
+        <IconButton className="p-3" size="large">
+          <ArrowBackIcon onClick={() => window.history.back()} />
+        </IconButton>
+        {singleProduct._id == id ? (
+          <Paper elevation={4} sx={{ width: { md: "100%", xs: "110%" } }}>
+            <div className="d-flex gap-4 flex-column flex-md-row p-3">
               <div className="">
-                <select
-                  style={{
-                    width: "100px",
-                    outline: "none",
-                    border: "1px solid grey",
-                    height: "30px",
-                    borderRadius: "5px",
-                    marginTop: "5px",
-                    color: "orange",
-                    fontWeight: 700,
-                    textAlign: "center",
-                    marginLeft: "70px",
-                  }}
-                  defaultValue="1"
-                >
-                  {Array.from(
-                    { length: singleProduct.stock },
-                    (_, index1) => index1 + 1
-                  ).map((quan, index2) => (
-                    <option value={quan} key={index2}>
-                      {quan}
-                    </option>
-                  ))}
-                  ;
-                </select>
-                <Button
-                  variant="outlined"
-                  startIcon={<ShoppingCart />}
-                  sx={{ marginLeft: "50px" }}
-                >
-                  Add to Cart
-                </Button>
+                <img
+                  src={singleProduct.image}
+                  style={{ width: "100%", height: "400px" }}
+                  className="p-3"
+                ></img>
               </div>
-              <div style={{ width: "300px", padding: "20px" }}>
-                <Button
-                  variant="contained"
-                  color="success"
-                  sx={{ width: "100%", padding: "10px", marginX: "70px" }}
+              <div>
+                <Typography variant="h3" className="fw-lighter p-3">
+                  {singleProduct.name}
+                </Typography>
+                <Divider textAlign="left">
+                  {" "}
+                  <Rating
+                    value={singleProduct.rating}
+                    readOnly
+                    size="medium"
+                    color="primary"
+                    className="ps-3"
+                  />
+                </Divider>
+
+                <Typography className="p-2" variant="h6">
+                  Description : {singleProduct.description}
+                </Typography>
+                <Typography className="p-2" variant="h4" sx={{ color: "red" }}>
+                  Price : {singleProduct.price + "/-"}
+                </Typography>
+                <Typography className="p-2" variant="h6" sx={{ color: "grey" }}>
+                  MRP incl. of all taxes
+                </Typography>
+                <Typography className="p-2" variant="h6" sx={{ color: "grey" }}>
+                  Color :{" "}
+                  <span style={{ color: singleProduct.color }}>
+                    {singleProduct.color}
+                  </span>
+                </Typography>
+                <Typography className="p-2" variant="h6" sx={{ color: "grey" }}>
+                  Category : {singleProduct.category}
+                </Typography>
+                <Typography
+                  className="p-2"
+                  variant="h6"
+                  sx={{ color: "black" }}
                 >
-                  Buy It now{" "}
-                </Button>
+                  Size :
+                </Typography>
+                <div className="ms-5">
+                  {singleProduct.size &&
+                    singleProduct.size.map((sizes, index) => (
+                      <>
+                        {sizes}
+                        <Checkbox
+                          key={index}
+                          value={sizes}
+                          onClick={(e) => Check(e)}
+                        />
+                      </>
+                    ))}
+                </div>
+                <Typography variant="h6">Quantity :</Typography>
+                <div className="">
+                  <select
+                    style={{
+                      width: "100px",
+                      outline: "none",
+                      border: "1px solid grey",
+                      height: "30px",
+                      borderRadius: "5px",
+                      marginTop: "5px",
+                      color: "orange",
+                      fontWeight: 700,
+                      textAlign: "center",
+                      marginLeft: "70px",
+                    }}
+                    defaultValue="1"
+                  >
+                    {Array.from(
+                      { length: singleProduct.stock },
+                      (_, index1) => index1 + 1
+                    ).map((quan, index2) => (
+                      <option value={quan} key={index2}>
+                        {quan}
+                      </option>
+                    ))}
+                    ;
+                  </select>
+                  <Button
+                    variant="outlined"
+                    startIcon={<ShoppingCart />}
+                    sx={{ marginLeft: "50px" }}
+                  >
+                    Add to Cart
+                  </Button>
+                </div>
+                <div style={{ width: "300px", padding: "20px" }}>
+                  <Button
+                    variant="contained"
+                    color="success"
+                    sx={{ width: "100%", padding: "10px", marginX: "70px" }}
+                  >
+                    Buy It now{" "}
+                  </Button>
+                </div>
               </div>
             </div>
+          </Paper>
+        ) : (
+          <div className="d-flex justify-content-center align-content-center">
+            <Spin tip="Loading" size="large"></Spin>
           </div>
-        </Paper>
+        )}
       </Container>
     </>
   );
